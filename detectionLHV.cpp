889a8e6467013562aaa379803d42c903881b0728
MaelaLaconi <44729941+MaelaLaconi@users.noreply.github.com>
2021-03-18 12:33:54 +0100

    refractor

---
+++ detectionLHV.cpp
@@ -0,0 +1,135 @@
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+double angleBetween(const Point& v1, const Point& v2)
+{
+    double len1 = sqrt(v1.x * v1.x + v1.y * v1.y);
+    double len2 = sqrt(v2.x * v2.x + v2.y * v2.y);
+
+    double dot = v1.x * v2.x + v1.y * v2.y;
+
+    double a = dot / (len1 * len2);
+
+    if (a >= 1.0)
+        return 0.0;
+    else if (a <= -1.0)
+        return 3.14; //retourner PI plus précis par la suite
+    else
+        return acos(a); // 0..PI
+}
+
+void getHorizontalLigne(int epsilon, vector<Vec4i> linesP, Mat cdstP){
+    // Draw the lines
+    for (size_t i = 0; i < linesP.size(); i++)
+    {
+        //ajouter ici pour gérer angle
+        Vec4i l = linesP[i];
+        Point point1 = Point(l[0], l[1]);
+        Point point2 = Point(l[2], l[3]);
+        int x = point1.x;
+        int y = point1.y;
+
+        int x2 = point2.x;
+        int y2 = point2.y;
+        if ((y2 <= y + epsilon && y2 >= y - epsilon)) {
+            line(cdstP, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, LINE_AA);
+        }
+      
+    }
+ 
+    //imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst);
+    imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP);
+    // Wait and Exit
+    waitKey();
+}
+
+void getVerticalLigne(int epsilon, vector<Vec4i> linesP, Mat cdstP) {
+    // Draw the lines
+    for (size_t i = 0; i < linesP.size(); i++)
+    {
+        //ajouter ici pour gérer angle
+        Vec4i l = linesP[i];
+        Point point1 = Point(l[0], l[1]);
+        Point point2 = Point(l[2], l[3]);
+        int x = point1.x;
+        int y = point1.y;
+
+        int x2 = point2.x;
+        int y2 = point2.y;
+        if ((x2 <= x + epsilon && x2 >= x - epsilon)) {
+            line(cdstP, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, LINE_AA);
+        }
+    }
+
+    //imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst);
+    imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP);
+    // Wait and Exit
+    waitKey();
+}
+
+int main(int argc, char** argv)
+{
+    char* select = new char[50];
+    printf("\n0. Selectionner lignes horizontales.\n1. Selectionner lignes verticales.\n");
+    cin >> select;
+
+    double epsilon = 50;
+    // Declare the output variables
+    Mat dst, cdst, cdstP;
+    const char* default_file = "sudoku.png";
+    const char* filename = argc >=2 ? argv[1] : default_file;
+    // Loads an image
+    Mat src = imread( samples::findFile( filename ), IMREAD_GRAYSCALE );
+    // Check if image is loaded fine
+    if(src.empty()){
+        printf(" Error opening image\n");
+        printf(" Program Arguments: [image_name -- default %s] \n", default_file);
+        return -1;
+    }
+    // Edge detection
+    Canny(src, dst, 50, 200, 3);
+    // Copy edges to the images that will display the results in BGR
+    cvtColor(dst, cdst, COLOR_GRAY2BGR);
+    cdstP = cdst.clone();
+ 
+    // Probabilistic Line Transform
+    vector<Vec4i> linesP; // will hold the results of the detection
+    HoughLinesP(dst, linesP, 1, CV_PI/180, 50, 50, 10 ); // runs the actual detection
+   
+    if (strcmp(select, "0") == 0) {
+        getHorizontalLigne(epsilon, linesP, cdstP);
+    }
+    if (strcmp(select, "1") == 0) {
+        getVerticalLigne(epsilon, linesP, cdstP);
+    }
+    // Show results
+    imshow("Source", src);
+    //imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst);
+    //imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP);
+    // Wait and Exit
+    waitKey();
+
+    
+
+    // CANNY
+
+    /*CommandLineParser parser(argc, argv, "{@input | Canny_Detector.jpg | input image}");
+    src = imread(samples::findFile(parser.get<String>("@input")), IMREAD_COLOR); // Load an image
+    if (src.empty())
+    {
+        std::cout << "Could not open or find the image!\n" << std::endl;
+        std::cout << "Usage: " << argv[0] << " <Input image>" << std::endl;
+        return -1;
+    }
+    dst.create(src.size(), src.type());
+    cvtColor(src, src_gray, COLOR_BGR2GRAY);
+    namedWindow(window_name, WINDOW_AUTOSIZE);
+    createTrackbar("Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold);
+    CannyThreshold(0, 0);
+    waitKey(0);*/
+    return 0;
+}
